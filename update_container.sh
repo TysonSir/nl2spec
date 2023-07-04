312bf499971f0d0c0d9f5f0dc348084012a22dd7
czz <czz890@qq.com>
2023-07-04 19:30:58 +0800

    修改文件名字

--- update_container.sh
+++ update_container.sh
@@ -1,3 +1,3 @@
 docker stop nl2spec && docker rm nl2spec && docker rmi nl2spec:latest
-docker build --tag nl2spec:latest --file artifact.Dockerfile . && docker run -v ~/nl2spec_vol:/workspace/nl2spec --name nl2spec -d -p 8001:5000 nl2spec:latest
+docker build --tag nl2spec:latest --file Dockerfile . && docker run -v ~/nl2spec_vol:/workspace/nl2spec --name nl2spec -d -p 8001:5000 nl2spec:latest
 docker logs nl2spec
